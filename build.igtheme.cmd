eef9d5626c4297a92d03f27486a066d6e07b0da0
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2023-08-20 06:52:31 +0900

    Update build.igtheme.cmd

--- build.igtheme.cmd
+++ build.igtheme.cmd
@@ -1,5 +1,6 @@
 @echo of
+cd /d "%~dp0"
 set /p <nul="Generating 'Dracula.igtheme'..."
-powershell.exe -noprofile -command try { Compress-Archive ./Dracula -Destination ./Dracula.igtheme -Force } catch { exit 1 }
-if %errorlevel% equ 0 (echo. done.) else (echo. error code: %errorlevel%)
+powershell.exe -noprofile -command try { Compress-Archive -Path ./Dracula -Destination ./dracula.igtheme -Force } catch { exit $lastexitcode }
+if %errorlevel% equ 0 (echo. done.) else (echo. error: %errorlevel%)
 exit /b %errorlevel%
